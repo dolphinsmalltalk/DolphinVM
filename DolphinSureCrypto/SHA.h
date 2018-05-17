d45715477285e14decc4bac36c89734c5f92faeb
FunctionPoint <richardronteltap@hotmail.com>
2018-05-17 23:12:09 +0200

    Native SHA1 BCrypt implementation
    DolphinSureCrypto.dll: Replacement of custom SHA1 implementation with native Windows BCrypt API.

--- DolphinSureCrypto/SHA.h
+++ DolphinSureCrypto/SHA.h
@@ -1,34 +1,24 @@
-#ifndef _SHA_DEFINED
+#pragma once
 
-#define _SHA_DEFINED
+// SHA1 Implementation using Windows BCrypt (CNG) API.
 
-/* The SHS block size and message digest sizes, in bytes */
+#include <Windows.h>
+#include <BCrypt.h>
 
-#include "Utils.h"
+class SHA
+{
+public:
+	SHA();
+	~SHA();
 
-#define SHA_DATASIZE	64
-#define SHA_DIGESTSIZE	20
+	void Create();
+	void Destroy();
 
-/* The structure for storing SHA info */
+	void HashBuffer( BYTE* buffer, int count );
+	void GetHash( BYTE* digest );
 
-typedef struct {
-			   DWORD digest[ 5 ];			/* Message digest */
-			   DWORD countLo, countHi;		/* 64-bit bit count */
-			   DWORD data[ 16 ];				/* SHS data buffer */
-#ifdef _BIG_WORDS
-			   BYTE dataBuffer[ SHA_DATASIZE ];	/* Byte buffer for data */
-#endif /* _BIG_WORDS */
-			   BOOLEAN done;				/* Whether final digest present */
-			   BOOLEAN isSHA;				/* Whether to use SHA */
-			   } SHA_INFO;
-
-/* Message digest functions */
-
-void shaInitial( SHA_INFO *shaInfo );
-void shaUpdate( SHA_INFO *shaInfo, BYTE *buffer, DWORD count );
-void shaFinal( SHA_INFO *shaInfo );
-void sha1Initial( SHA_INFO *shaInfo );
-void sha1Update( SHA_INFO *shaInfo, BYTE *buffer, DWORD count );
-void sha1Final( SHA_INFO *shaInfo );
-
-#endif /* _SHA_DEFINED */
+private:
+	const DWORD hashSize = 20;
+    static BCRYPT_ALG_HANDLE algHandle;
+	BCRYPT_HASH_HANDLE hashHandle;
+};
